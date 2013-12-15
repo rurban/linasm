b89452f0b278c1f0d8528f8b47442be0ea1c1997
Jack Black <ezamlinsky@users.sf.net>
2013-12-16 00:57:53 +0200

    Angle.h: Standardized functions namespace

--- include/Angle.h
+++ include/Angle.h
@@ -11,24 +11,14 @@
 # include	<Types.h>
 # include	<Math.h>
 
-# ifdef	__cplusplus
-/*
-################################################################################
-#       C++ prototypes                                                         #
-################################################################################
-*/
-class Angle
-{
-public:
-
 //****************************************************************************//
 //      Conversion radians to degrees                                         //
 //****************************************************************************//
-static inline flt32_t RadToDeg (flt32_t angle)
+inline flt32_t RadToDeg (flt32_t angle)
 {
 	return angle * (180.0 / M_PI);
 }
-static inline flt64_t RadToDeg (flt64_t angle)
+inline flt64_t RadToDeg (flt64_t angle)
 {
 	return angle * (180.0 / M_PI);
 }
@@ -36,89 +26,11 @@ static inline flt64_t RadToDeg (flt64_t angle)
 //****************************************************************************//
 //      Conversion radians to gradians                                        //
 //****************************************************************************//
-static inline flt32_t RadToGrad (flt32_t angle)
+inline flt32_t RadToGrad (flt32_t angle)
 {
 	return angle * (200.0 / M_PI);
 }
-static inline flt64_t RadToGrad (flt64_t angle)
-{
-	return angle * (200.0 / M_PI);
-}
-
-//****************************************************************************//
-//      Conversion degrees to radians                                         //
-//****************************************************************************//
-static inline flt32_t DegToRad (flt32_t angle)
-{
-	return angle * (M_PI / 180.0);
-}
-static inline flt64_t DegToRad (flt64_t angle)
-{
-	return angle * (M_PI / 180.0);
-}
-
-//****************************************************************************//
-//      Conversion degrees to gradians                                        //
-//****************************************************************************//
-static inline flt32_t DegToGrad (flt32_t angle)
-{
-	return angle * (200.0 / 180.0);
-}
-static inline flt64_t DegToGrad (flt64_t angle)
-{
-	return angle * (200.0 / 180.0);
-}
-
-//****************************************************************************//
-//      Conversion gradians to radians                                        //
-//****************************************************************************//
-static inline flt32_t GradToRad (flt32_t angle)
-{
-	return angle * (M_PI / 200.0);
-}
-static inline flt64_t GradToRad (flt64_t angle)
-{
-	return angle * (M_PI / 200.0);
-}
-
-//****************************************************************************//
-//      Conversion gradians to degrees                                        //
-//****************************************************************************//
-static inline flt32_t GradToDeg (flt32_t angle)
-{
-	return angle * (180.0 / 200.0);
-}
-static inline flt64_t GradToDeg (flt64_t angle)
-{
-	return angle * (180.0 / 200.0);
-}
-};
-# else
-/*
-################################################################################
-#       C prototypes                                                           #
-################################################################################
-*/
-//****************************************************************************//
-//      Conversion radians to degrees                                         //
-//****************************************************************************//
-inline flt32_t Angle_RadToDeg_flt32 (flt32_t angle)
-{
-	return angle * (180.0 / M_PI);
-}
-inline flt64_t Angle_RadToDeg_flt64 (flt64_t angle)
-{
-	return angle * (180.0 / M_PI);
-}
-
-//****************************************************************************//
-//      Conversion radians to gradians                                        //
-//****************************************************************************//
-inline flt32_t Angle_RadToGrad_flt32 (flt32_t angle)
-{
-	return angle * (200.0 / M_PI);
-}
-inline flt64_t Angle_RadToGrad_flt64 (flt64_t angle)
+inline flt64_t RadToGrad (flt64_t angle)
 {
 	return angle * (200.0 / M_PI);
 }
@@ -126,11 +38,11 @@ inline flt64_t Angle_RadToGrad_flt64 (flt64_t angle)
 //****************************************************************************//
 //      Conversion degrees to radians                                         //
 //****************************************************************************//
-inline flt32_t Angle_DegToRad_flt32 (flt32_t angle)
+inline flt32_t DegToRad (flt32_t angle)
 {
 	return angle * (M_PI / 180.0);
 }
-inline flt64_t Angle_DegToRad_flt64 (flt64_t angle)
+inline flt64_t DegToRad (flt64_t angle)
 {
 	return angle * (M_PI / 180.0);
 }
@@ -138,11 +50,11 @@ inline flt64_t Angle_DegToRad_flt64 (flt64_t angle)
 //****************************************************************************//
 //      Conversion degrees to gradians                                        //
 //****************************************************************************//
-inline flt32_t Angle_DegToGrad_flt32 (flt32_t angle)
+inline flt32_t DegToGrad (flt32_t angle)
 {
 	return angle * (200.0 / 180.0);
 }
-inline flt64_t Angle_DegToGrad_flt64 (flt64_t angle)
+inline flt64_t DegToGrad (flt64_t angle)
 {
 	return angle * (200.0 / 180.0);
 }
@@ -150,11 +62,11 @@ inline flt64_t Angle_DegToGrad_flt64 (flt64_t angle)
 //****************************************************************************//
 //      Conversion gradians to radians                                        //
 //****************************************************************************//
-inline flt32_t Angle_GradToRad_flt32 (flt32_t angle)
+inline flt32_t GradToRad (flt32_t angle)
 {
 	return angle * (M_PI / 200.0);
 }
-inline flt64_t Angle_GradToRad_flt64 (flt64_t angle)
+inline flt64_t GradToRad (flt64_t angle)
 {
 	return angle * (M_PI / 200.0);
 }
@@ -162,15 +74,14 @@ inline flt64_t Angle_GradToRad_flt64 (flt64_t angle)
 //****************************************************************************//
 //      Conversion gradians to degrees                                        //
 //****************************************************************************//
-inline flt32_t Angle_GradToDeg_flt32 (flt32_t angle)
+inline flt32_t GradToDeg (flt32_t angle)
 {
 	return angle * (180.0 / 200.0);
 }
-inline flt64_t Angle_GradToDeg_flt64 (flt64_t angle)
+inline flt64_t GradToDeg (flt64_t angle)
 {
 	return angle * (180.0 / 200.0);
 }
-# endif
 /*
 ################################################################################
 #                                 END OF FILE                                  #
