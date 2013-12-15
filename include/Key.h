d0a2c21c6f23819ece0e23af21583a9d176d2c03
Jack Black <ezamlinsky@users.sf.net>
2013-12-16 01:41:42 +0200

    Added Stack ADT

---
+++ include/Key.h
@@ -0,0 +1,75 @@
+/*                                                                         Key.h
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                    KEY STRUCTURE FOR ABSTRACT DATA TYPES                     #
+#                                                                              #
+# License: LGPLv3+                               Copyleft (Ɔ) 2013, Jack Black #
+################################################################################
+*/
+# pragma	once
+# include	<Types.h>
+
+//****************************************************************************//
+//      Abstact data type union                                               //
+//****************************************************************************//
+union adt_t
+{
+	// Unsigned integer keys
+	uint8_t		uint8;
+	uint16_t	uint16;
+	uint32_t	uint32;
+	uint64_t	uint64;
+
+	// Signed integer keys
+	sint8_t		sint8;
+	sint16_t	sint16;
+	sint32_t	sint32;
+	sint64_t	sint64;
+
+	// Floating-point types
+	flt32_t		flt32;
+	flt64_t		flt64;
+
+	// Pointer type
+	void		*ptr;
+};
+
+//****************************************************************************//
+//      Data structure                                                        //
+//****************************************************************************//
+struct data_t
+{
+	union adt_t	key;	// Key field
+	union adt_t	data;	// Data field
+};
+
+//****************************************************************************//
+//      Key compare function prototype                                        //
+//****************************************************************************//
+typedef sint64_t (*KeyCmp) (union adt_t key1, union adt_t key2);
+
+//****************************************************************************//
+//      Key compare functions                                                 //
+//****************************************************************************//
+
+// Unsigned integer keys
+sint64_t CmpUint8 (union adt_t key1, union adt_t key2);
+sint64_t CmpUint16 (union adt_t key1, union adt_t key2);
+sint64_t CmpUint32 (union adt_t key1, union adt_t key2);
+sint64_t CmpUint64 (union adt_t key1, union adt_t key2);
+
+// Signed integer keys
+sint64_t CmpSint8 (union adt_t key1, union adt_t key2);
+sint64_t CmpSint16 (union adt_t key1, union adt_t key2);
+sint64_t CmpSint32 (union adt_t key1, union adt_t key2);
+sint64_t CmpSint64 (union adt_t key1, union adt_t key2);
+
+// Floating-point types
+sint64_t CmpFlt32 (union adt_t key1, union adt_t key2);
+sint64_t CmpFlt64 (union adt_t key1, union adt_t key2);
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
---
+++ include/Stack.h
@@ -0,0 +1,252 @@
+/*                                                                       Stack.h
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                               STACK DATA TYPE                                #
+#                                                                              #
+# License: LGPLv3+                               Copyleft (Ɔ) 2013, Jack Black #
+################################################################################
+*/
+# pragma	once
+# include	<Key.h>
+
+# ifdef	__cplusplus
+/*
+################################################################################
+#       C++ prototypes                                                         #
+################################################################################
+*/
+//****************************************************************************//
+//      Stack class                                                           //
+//****************************************************************************//
+class Stack
+{
+private:
+	void	*array;		// Pointer to array which holds stack data
+	size_t	capacity;	// Capacity of the stack (auto extended if required)
+	size_t	size;		// Current stack size
+
+public:
+
+//****************************************************************************//
+//      Constructor                                                           //
+//****************************************************************************//
+Stack (size_t capacity);
+
+//****************************************************************************//
+//      Copy constructor                                                      //
+//****************************************************************************//
+Stack (const Stack &source);
+
+//****************************************************************************//
+//      Destructor                                                            //
+//****************************************************************************//
+~Stack (void);
+
+//****************************************************************************//
+//      Copying elements                                                      //
+//****************************************************************************//
+size_t Copy (const Stack *source, size_t pos, size_t count);
+
+//****************************************************************************//
+//      Moving elements                                                       //
+//****************************************************************************//
+size_t Move (Stack *source, size_t pos, size_t count);
+
+//****************************************************************************//
+//      Insertion of element                                                  //
+//****************************************************************************//
+bool Push (const data_t *data);
+
+//****************************************************************************//
+//      Removing of element                                                   //
+//****************************************************************************//
+bool Pop (data_t *data);
+
+//****************************************************************************//
+//      Setting element value                                                 //
+//****************************************************************************//
+bool Set (const data_t *data, size_t pos);
+
+//****************************************************************************//
+//      Getting element value                                                 //
+//****************************************************************************//
+bool Get (data_t *data, size_t pos) const;
+
+//****************************************************************************//
+//      Replacing element value                                               //
+//****************************************************************************//
+bool Replace (data_t *odata, const data_t *ndata, size_t pos);
+
+//****************************************************************************//
+//      Changing elements order                                               //
+//****************************************************************************//
+size_t Reverse (size_t pos, size_t count);
+bool Swap (size_t pos);
+
+//****************************************************************************//
+//      Minimum and maximum value                                             //
+//****************************************************************************//
+size_t Min (data_t *data, size_t pos, size_t count, KeyCmp func) const;
+size_t Max (data_t *data, size_t pos, size_t count, KeyCmp func) const;
+
+//****************************************************************************//
+//      Search algorithms                                                     //
+//****************************************************************************//
+
+//============================================================================//
+//      Key searching                                                         //
+//============================================================================//
+size_t FindKey (data_t *data, adt_t key, size_t pos, size_t count, KeyCmp func) const;
+size_t FindKeys (data_t *data, const adt_t keys[], size_t size, size_t pos, size_t count, KeyCmp func) const;
+
+//============================================================================//
+//      Searching for differences                                             //
+//============================================================================//
+size_t FindDiff (data_t *data, const Stack *source, size_t pos, size_t count, KeyCmp func) const;
+
+//****************************************************************************//
+//      Key counting                                                          //
+//****************************************************************************//
+size_t CountKey (adt_t key, size_t pos, size_t count, KeyCmp func) const;
+size_t CountKeys (const adt_t keys[], size_t size, size_t pos, size_t count, KeyCmp func) const;
+
+//****************************************************************************//
+//      Comparison of stacks                                                  //
+//****************************************************************************//
+sint64_t Compare (const Stack *source, KeyCmp func) const;
+
+//****************************************************************************//
+//      Check for equality                                                    //
+//****************************************************************************//
+bool IsEqual (const Stack *source, KeyCmp func) const;
+
+//****************************************************************************//
+//      Stack properties                                                      //
+//****************************************************************************//
+size_t Capacity (void) const;
+size_t Size (void) const;
+bool IsEmpty (void) const;
+};
+# else
+/*
+################################################################################
+#       C prototypes                                                           #
+################################################################################
+*/
+//****************************************************************************//
+//      Stack structure                                                       //
+//****************************************************************************//
+struct Stack
+{
+	void	*array;		// Pointer to array which holds stack data
+	size_t	capacity;	// Capacity of the stack (auto extended if required)
+	size_t	size;		// Current stack size
+};
+
+//****************************************************************************//
+//      Init stack structure                                                  //
+//****************************************************************************//
+void Stack_InitStack (struct Stack *stack, size_t capacity);
+
+//****************************************************************************//
+//      Copy stack structure                                                  //
+//****************************************************************************//
+void Stack_CopyStack (struct Stack *stack, const struct Stack *source);
+
+//****************************************************************************//
+//      Free stack structure                                                  //
+//****************************************************************************//
+void Stack_FreeStack (struct Stack *stack);
+
+//****************************************************************************//
+//      Copying elements                                                      //
+//****************************************************************************//
+size_t Stack_Copy (struct Stack *stack, const struct Stack *source, size_t pos, size_t count);
+
+//****************************************************************************//
+//      Moving elements                                                       //
+//****************************************************************************//
+size_t Stack_Move (struct Stack *stack, struct Stack *source, size_t pos, size_t count);
+
+//****************************************************************************//
+//      Insertion of element                                                  //
+//****************************************************************************//
+bool Stack_Push (struct Stack *stack, const struct data_t *data);
+
+//****************************************************************************//
+//      Removing of element                                                   //
+//****************************************************************************//
+bool Stack_Pop (struct Stack *stack, struct data_t *data);
+
+//****************************************************************************//
+//      Setting element value                                                 //
+//****************************************************************************//
+bool Stack_Set (struct Stack *stack, const struct data_t *data, size_t pos);
+
+//****************************************************************************//
+//      Getting element value                                                 //
+//****************************************************************************//
+bool Stack_Get (const struct Stack *stack, struct data_t *data, size_t pos);
+
+//****************************************************************************//
+//      Replacing element value                                               //
+//****************************************************************************//
+bool Stack_Replace (struct Stack *stack, struct data_t *odata, const struct data_t *ndata, size_t pos);
+
+//****************************************************************************//
+//      Changing elements order                                               //
+//****************************************************************************//
+size_t Stack_Reverse (struct Stack *stack, size_t pos, size_t count);
+bool Stack_Swap (struct Stack *stack, size_t pos);
+
+//****************************************************************************//
+//      Minimum and maximum value                                             //
+//****************************************************************************//
+size_t Stack_Min (const struct Stack *stack, struct data_t *data, size_t pos, size_t count, KeyCmp func);
+size_t Stack_Max (const struct Stack *stack, struct data_t *data, size_t pos, size_t count, KeyCmp func);
+
+//****************************************************************************//
+//      Search algorithms                                                     //
+//****************************************************************************//
+
+//============================================================================//
+//      Key searching                                                         //
+//============================================================================//
+size_t Stack_FindKey (const struct Stack *stack, struct data_t *data, union adt_t key, size_t pos, size_t count, KeyCmp func);
+size_t Stack_FindKeys (const struct Stack *stack, struct data_t *data, const union adt_t keys[], size_t size, size_t pos, size_t count, KeyCmp func);
+
+//============================================================================//
+//      Searching for differences                                             //
+//============================================================================//
+size_t Stack_FindDiff (const struct Stack *stack, struct data_t *data, const struct Stack *source, size_t pos, size_t count, KeyCmp func);
+
+//****************************************************************************//
+//      Key counting                                                          //
+//****************************************************************************//
+size_t Stack_CountKey (const struct Stack *stack, union adt_t key, size_t pos, size_t count, KeyCmp func);
+size_t Stack_CountKeys (const struct Stack *stack, const union adt_t keys[], size_t size, size_t pos, size_t count, KeyCmp func);
+
+//****************************************************************************//
+//      Comparison of stacks                                                  //
+//****************************************************************************//
+sint64_t Stack_Compare (const struct Stack *stack, const struct Stack *source, KeyCmp func);
+
+//****************************************************************************//
+//      Check for equality                                                    //
+//****************************************************************************//
+bool Stack_IsEqual (const struct Stack *stack, const struct Stack *source, KeyCmp func);
+
+//****************************************************************************//
+//      Stack properties                                                      //
+//****************************************************************************//
+size_t Stack_Capacity (const struct Stack *stack);
+size_t Stack_Size (const struct Stack *stack);
+bool Stack_IsEmpty (const struct Stack *stack);
+
+# endif
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
