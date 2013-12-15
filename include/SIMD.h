f35ee13a3687e0eb0d66ee1ecf78a22467771553
Jack Black <ezamlinsky@users.sf.net>
2013-12-16 01:38:07 +0200

    Added SIMD control library

---
+++ include/SIMD.h
@@ -0,0 +1,212 @@
+/*                                                                        SIMD.h
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                            SIMD CONTROL FUNCTIONS                            #
+#                                                                              #
+# License: LGPLv3+                               Copyleft (Ɔ) 2013, Jack Black #
+################################################################################
+*/
+# pragma	once
+
+//****************************************************************************//
+//      SIMD constants                                                        //
+//****************************************************************************//
+
+// SIMD rounding modes
+# define	SIMD_RC_NEAREST		0x0000		// Round to nearest even integer
+# define	SIMD_RC_ROND_DOWN	0x0001		// Round down (floor)
+# define	SIMD_RC_ROND_UP		0x0002		// Round up (ceil)
+# define	SIMD_RC_TRUNCATE	0x0003		// Round to nearest integer, toward zero (truncation)
+
+// SIMD exception masks
+# define	SIMD_PM				0x0020		// Precision mask
+# define	SIMD_UM				0x0010		// Underflow mask
+# define	SIMD_OM				0x0008		// Overflow mask
+# define	SIMD_ZM				0x0004		// Divide by zero mask
+# define	SIMD_DM				0x0002		// Denormal operand mask
+# define	SIMD_IM				0x0001		// Invalid operation mask
+# define	SIMD_ALL_MASKS		0x003F		// All masks
+
+// SIMD exception flags
+# define	SIMD_PE				0x0020		// Precision flag
+# define	SIMD_UE				0x0010		// Underflow flag
+# define	SIMD_OE				0x0008		// Overflow flag
+# define	SIMD_ZE				0x0004		// Divide by zero flag
+# define	SIMD_DE				0x0002		// Denormal operand flag
+# define	SIMD_IE				0x0001		// Invalid operation flag
+# define	SIMD_ALL_EXCPTS		0x003F		// All flags
+
+// SIMD defaults
+# define	SIMD_DEFAULT		0x007E		// Default SIMD mode
+
+//****************************************************************************//
+//      SIMD data types                                                       //
+//****************************************************************************//
+# define	simd_ctrl_t			int			// SIMD control word
+# define	simd_stat_t			int			// SIMD status word
+
+//****************************************************************************//
+//      SIMD control functions                                                //
+//****************************************************************************//
+
+//============================================================================//
+//      SIMD rounding mode                                                    //
+//============================================================================//
+
+// Set round mode
+inline simd_ctrl_t SetRoundMode (simd_ctrl_t cword, int rmode)
+{
+	return cword & ~(0x3 << 7) | rmode << 7;
+}
+
+// Get round mode
+inline int GetRoundMode (simd_ctrl_t cword)
+{
+	return (cword & (0x3 << 7)) >> 7;
+}
+
+//============================================================================//
+//      SIMD exception masks                                                  //
+//============================================================================//
+
+// Clear exception masks
+inline simd_ctrl_t ClearExceptionMasks (simd_ctrl_t cword, int exceptions)
+{
+	return cword & ~(exceptions << 1);
+}
+
+// Set exception masks
+inline simd_ctrl_t SetExceptionMasks (simd_ctrl_t cword, int exceptions)
+{
+	return cword | (exceptions << 1);
+}
+
+// Get exception masks
+inline int GetExceptionMasks (simd_ctrl_t cword, int exceptions)
+{
+	return (cword & (exceptions << 1)) >> 1;
+}
+
+//============================================================================//
+//      SIMD flush to zero mode                                               //
+//============================================================================//
+
+// Clear flush to zero flag
+inline simd_ctrl_t ClearFlushToZeroFlag (simd_ctrl_t cword)
+{
+	return cword & ~(0x1 << 9);
+}
+
+// Set flush to zero flag
+inline simd_ctrl_t SetFlushToZeroFlag (simd_ctrl_t cword)
+{
+	return cword | (0x1 << 9);
+}
+
+// Get flush to zero flag
+inline bool GetFlushToZeroFlag (simd_ctrl_t cword)
+{
+	return (cword & (0x1 << 9)) >> 9;
+}
+
+//============================================================================//
+//      SIMD denormals are zeros                                              //
+//============================================================================//
+
+// Clear denormals are zeros flag
+inline simd_ctrl_t ClearDenormalsAreZerosFlag (simd_ctrl_t cword)
+{
+	return cword & ~0x1;
+}
+
+// Set denormals are zeros flag
+inline simd_ctrl_t SetDenormalsAreZerosFlag (simd_ctrl_t cword)
+{
+	return cword | 0x1;
+}
+
+// Get denormals are zeros flag
+inline bool GetDenormalsAreZerosFlag (simd_ctrl_t cword)
+{
+	return cword & 0x1;
+}
+
+//****************************************************************************//
+//      SIMD status functions                                                 //
+//****************************************************************************//
+
+// SIMD exception flags
+inline int GetExceptions (simd_stat_t sword, int exceptions)
+{
+	return sword & exceptions;
+}
+
+# ifdef	__cplusplus
+/*
+################################################################################
+#       C++ prototypes                                                         #
+################################################################################
+*/
+class SIMD
+{
+public:
+
+//****************************************************************************//
+//      SIMD control functions                                                //
+//****************************************************************************//
+
+// Init SIMD
+static void Init (void);
+
+// Set SIMD mode
+static void SetMode (simd_ctrl_t cword);
+
+// Get SIMD mode
+static simd_ctrl_t GetMode (void);
+
+//****************************************************************************//
+//      SIMD status functions                                                 //
+//****************************************************************************//
+
+// Clear SIMD exceptions
+static void ClearExceptions (void);
+
+// Get SIMD status
+static simd_stat_t GetStatus (void);
+};
+# else
+/*
+################################################################################
+#       C prototypes                                                           #
+################################################################################
+*/
+//****************************************************************************//
+//      SIMD control functions                                                //
+//****************************************************************************//
+
+// Init SIMD
+void SIMD_Init (void);
+
+// Set SIMD mode
+void SIMD_SetMode (simd_ctrl_t cword);
+
+// Get SIMD mode
+simd_ctrl_t SIMD_GetMode (void);
+
+//****************************************************************************//
+//      SIMD status functions                                                 //
+//****************************************************************************//
+
+// Clear SIMD exceptions
+void SIMD_ClearExceptions (void);
+
+// Get SIMD status
+simd_stat_t SIMD_GetStatus (void);
+
+# endif
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
---
+++ source/SIMD.asm
@@ -0,0 +1,132 @@
+;                                                                       SIMD.asm
+;###############################################################################
+;# Encoding: UTF-8                                                 Tab size: 4 #
+;#                                                                             #
+;#                           SIMD CONTROL FUNCTIONS                            #
+;#                                                                             #
+;# License: LGPLv3+                              Copyleft (Ɔ) 2013, Jack Black #
+;###############################################################################
+format	ELF64
+
+;###############################################################################
+;#      Export section                                                         #
+;###############################################################################
+
+;******************************************************************************;
+;       SIMD control functions                                                 ;
+;******************************************************************************;
+
+; Init SIMD
+public	Init						as	'SIMD_Init'
+public	Init						as	'_ZN4SIMD4InitEv'
+
+; Set SIMD mode
+public	SetMode						as	'SIMD_SetMode'
+public	SetMode						as	'_ZN4SIMD7SetModeEi'
+
+; Get SIMD mode
+public	GetMode						as	'SIMD_GetMode'
+public	GetMode						as	'_ZN4SIMD7GetModeEv'
+
+;******************************************************************************;
+;       SIMD status functions                                                  ;
+;******************************************************************************;
+
+; Clear SIMD exceptions
+public	ClearExceptions				as	'SIMD_ClearExceptions'
+public	ClearExceptions				as	'_ZN4SIMD15ClearExceptionsEv'
+
+; Get SIMD status
+public	GetStatus					as	'SIMD_GetStatus'
+public	GetStatus					as	'_ZN4SIMD9GetStatusEv'
+
+;###############################################################################
+;#      Code section                                                           #
+;###############################################################################
+section	'.text'		executable align 16
+
+;******************************************************************************;
+;       Consts                                                                 ;
+;******************************************************************************;
+DEFAULT		= 0x1F80						; Default SIMD mode
+CMASK		= 0x03FF						; Mask to extract SIMD control word
+SMASK		= 0x003F						; Mask to extract SIMD status word
+
+;******************************************************************************;
+;       SIMD control functions                                                 ;
+;******************************************************************************;
+
+;==============================================================================;
+;       Init SIMD                                                              ;
+;==============================================================================;
+Init:
+;---[Internal variables]-------------------
+mxcsr	equ		rsp - 1 * 8					; stack position to store MXCSR register
+;------------------------------------------
+		mov		dword [mxcsr], DEFAULT		; set default SIMD mode
+		ldmxcsr	[mxcsr]						; load MXCSR register from the stack
+		ret
+
+;==============================================================================;
+;       Set SIMD mode                                                          ;
+;==============================================================================;
+SetMode:
+;---[Parameters]---------------------------
+cword	equ		edi							; SIMD control word
+;---[Internal variables]-------------------
+mxcsr	equ		rsp - 1 * 8					; stack position to store MXCSR register
+;------------------------------------------
+		stmxcsr	[mxcsr]						; store MXCSR register into the stack
+		and		cword, CMASK				; clear unrequired bits into control word
+		shl		cword, 6					; cword <<= 6
+		and		dword [mxcsr], not (CMASK shl 6)
+		or		[mxcsr], cword				; set new value of MXCSR register
+		ldmxcsr	[mxcsr]						; load MXCSR register from the stack
+		ret
+
+;==============================================================================;
+;       Get SIMD mode                                                          ;
+;==============================================================================;
+GetMode:
+;---[Internal variables]-------------------
+cword	equ		eax							; SIMD control word
+mxcsr	equ		rsp - 1 * 8					; stack position to store MXCSR register
+;------------------------------------------
+		stmxcsr	[mxcsr]						; store MXCSR register into the stack
+		mov		cword, [mxcsr]				; get value of MXCSR register
+		shr		cword, 6					; cword >>= 6
+		and		cword, CMASK				; clear unrequired bits into control word
+		ret									; return SIMD control word
+
+;******************************************************************************;
+;       SIMD status functions                                                  ;
+;******************************************************************************;
+
+;==============================================================================;
+;       Clear SIMD exceptions                                                  ;
+;==============================================================================;
+ClearExceptions:
+;---[Internal variables]-------------------
+mxcsr	equ		rsp - 1 * 8					; stack position to store MXCSR register
+;------------------------------------------
+		stmxcsr	[mxcsr]						; store MXCSR register into the stack
+		and		dword [mxcsr], not SMASK	; clear old value of status word
+		ldmxcsr	[mxcsr]						; load MXCSR register from the stack
+		ret
+
+;==============================================================================;
+;       Get SIMD status                                                        ;
+;==============================================================================;
+GetStatus:
+;---[Internal variables]-------------------
+sword	equ		eax							; SIMD status word
+mxcsr	equ		rsp - 1 * 8					; stack position to store MXCSR register
+;------------------------------------------
+		stmxcsr	[mxcsr]						; store MXCSR register into the stack
+		mov		sword, [mxcsr]				; get value of MXCSR register
+		and		sword, SMASK				; clear unrequired bits into status word
+		ret									; return SIMD status word
+
+;###############################################################################
+;#                                 END OF FILE                                 #
+;###############################################################################
