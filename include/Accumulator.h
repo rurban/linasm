a63d26e39c4b3f15a786d5baa66d47acc7dbb6f6
Jack Black <ezamlinsky@users.sf.net>
2013-12-16 01:39:35 +0200

    Added Accumulating buffer library

---
+++ include/Accumulator.h
@@ -0,0 +1,112 @@
+/*                                                                 Accumulator.h
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                             ACCUMULATING BUFFER                              #
+#                                                                              #
+# License: LGPLv3+                               Copyleft (Ɔ) 2013, Jack Black #
+################################################################################
+*/
+# pragma	once
+# include	<Types.h>
+
+# ifdef	__cplusplus
+/*
+################################################################################
+#       C++ prototypes                                                         #
+################################################################################
+*/
+//****************************************************************************//
+//      Accumulating buffer class                                             //
+//****************************************************************************//
+class Accumulator
+{
+private:
+	void	*buffer;	// Pointer to memory buffer
+	size_t	capacity;	// Capacity of the buffer (auto extended if required)
+	size_t	size;		// Current buffer size
+
+public:
+
+//****************************************************************************//
+//      Constructor                                                           //
+//****************************************************************************//
+Accumulator (size_t capacity);
+
+//****************************************************************************//
+//      Copy constructor                                                      //
+//****************************************************************************//
+Accumulator (const Accumulator &source);
+
+//****************************************************************************//
+//      Destructor                                                            //
+//****************************************************************************//
+~Accumulator (void);
+
+//****************************************************************************//
+//      Accumulator functions                                                 //
+//****************************************************************************//
+void* Reserve (size_t size);
+bool Fill (size_t size);
+const void* Data (void) const;
+void Clear (void);
+
+//****************************************************************************//
+//      Accumulator properties                                                //
+//****************************************************************************//
+size_t Capacity (void) const;
+size_t Size (void) const;
+bool IsEmpty (void) const;
+};
+# else
+/*
+################################################################################
+#       C prototypes                                                           #
+################################################################################
+*/
+//****************************************************************************//
+//      Accumulating buffer structure                                         //
+//****************************************************************************//
+struct Accumulator
+{
+	void	*buffer;	// Pointer to memory buffer
+	size_t	capacity;	// Capacity of the buffer (auto extended if required)
+	size_t	size;		// Current buffer size
+};
+
+//****************************************************************************//
+//      Init accumulator structure                                            //
+//****************************************************************************//
+void Accumulator_InitAccumulator (struct Accumulator *accumulator, size_t capacity);
+
+//****************************************************************************//
+//      Copy accumulator structure                                            //
+//****************************************************************************//
+void Accumulator_CopyAccumulator (struct Accumulator *accumulator, const struct Accumulator *source);
+
+//****************************************************************************//
+//      Free accumulator structure                                            //
+//****************************************************************************//
+void Accumulator_FreeAccumulator (struct Accumulator *accumulator);
+
+//****************************************************************************//
+//      Accumulator functions                                                 //
+//****************************************************************************//
+void* Accumulator_Reserve (struct Accumulator *accumulator, size_t size);
+bool Accumulator_Fill (struct Accumulator *accumulator, size_t size);
+const void* Accumulator_Data (const struct Accumulator *accumulator);
+void Accumulator_Clear (struct Accumulator *accumulator);
+
+//****************************************************************************//
+//      Accumulator properties                                                //
+//****************************************************************************//
+size_t Accumulator_Capacity (const struct Accumulator *accumulator);
+size_t Accumulator_Size (const struct Accumulator *accumulator);
+bool Accumulator_IsEmpty (const struct Accumulator *accumulator);
+
+# endif
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
---
+++ source/Accumulator.asm
@@ -0,0 +1,357 @@
+;                                                                Accumulator.asm
+;###############################################################################
+;# Encoding: UTF-8                                                 Tab size: 4 #
+;#                                                                             #
+;#                             ACCUMULATING BUFFER                             #
+;#                                                                             #
+;# License: LGPLv3+                              Copyleft (Ɔ) 2013, Jack Black #
+;###############################################################################
+format	ELF64
+include	'Macro.inc'
+include	'Syscall.inc'
+
+;###############################################################################
+;#      Import section                                                         #
+;###############################################################################
+extrn	'Array_Copy'		as	Copy
+
+;###############################################################################
+;#      Export section                                                         #
+;###############################################################################
+
+;******************************************************************************;
+;       Constructor                                                            ;
+;******************************************************************************;
+public	Constructor			as	'Accumulator_InitAccumulator'
+public	Constructor			as	'_ZN11AccumulatorC1Em'
+
+;******************************************************************************;
+;       Copy constructor                                                       ;
+;******************************************************************************;
+public	CopyConstructor		as	'Accumulator_CopyAccumulator'
+public	CopyConstructor		as	'_ZN11AccumulatorC1ERKS_'
+
+;******************************************************************************;
+;       Destructor                                                             ;
+;******************************************************************************;
+public	Destructor			as	'Accumulator_FreeAccumulator'
+public	Destructor			as	'_ZN11AccumulatorD1Ev'
+
+;******************************************************************************;
+;       Accumulator functions                                                  ;
+;******************************************************************************;
+
+; Reserve space into the buffer
+public	Reserve				as	'Accumulator_Reserve'
+public	Reserve				as	'_ZN11Accumulator7ReserveEm'
+
+; Fill reservered space into the buffer
+public	Fill				as	'Accumulator_Fill'
+public	Fill				as	'_ZN11Accumulator4FillEm'
+
+; Get pointer to the buffer
+public	GetData				as	'Accumulator_Data'
+public	GetData				as	'_ZNK11Accumulator4DataEv'
+
+; Clear the buffer
+public	Clear				as	'Accumulator_Clear'
+public	Clear				as	'_ZN11Accumulator5ClearEv'
+
+;******************************************************************************;
+;       Accumulator properties                                                 ;
+;******************************************************************************;
+
+; Accumulator capacity
+public	GetCapacity			as	'Accumulator_Capacity'
+public	GetCapacity			as	'_ZNK11Accumulator8CapacityEv'
+
+; Accumulator size
+public	GetSize				as	'Accumulator_Size'
+public	GetSize				as	'_ZNK11Accumulator4SizeEv'
+
+; Check if accumulator is empty
+public	IsEmpty				as	'Accumulator_IsEmpty'
+public	IsEmpty				as	'_ZNK11Accumulator7IsEmptyEv'
+
+;###############################################################################
+;#      Code section                                                           #
+;###############################################################################
+section	'.text'		executable align 16
+
+;******************************************************************************;
+;       Consts                                                                 ;
+;******************************************************************************;
+BUFFER		= 0 * 8							; Offset of buffer pointer
+CAPACITY	= 1 * 8							; Offset of object capacity field
+SIZE		= 2 * 8							; Offset of object size field
+
+;******************************************************************************;
+;       Extending accumulator capacity                                         ;
+;******************************************************************************;
+Extend:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+newcap	equ		rsi							; new object capacity
+;---[Internal variables]-------------------
+status	equ		al							; operation status
+buffer	equ		rax							; pointer to memory buffer
+stack	equ		rsp							; stack pointer
+s_this	equ		stack + 0 * 8				; stack position of "this" variable
+s_ncap	equ		stack + 1 * 8				; stack position of "newcap" variable
+space	= 3 * 8								; stack size required by the procedure
+;------------------------------------------
+		sub		stack, space				; reserving stack size for local vars
+		mov		[s_this], this				; save "this" variable into the stack
+		mov		[s_ncap], newcap			; save "newcap" variable into the stack
+;---[Reallocate memory for the object]-----
+		mov		sc_prm4, 1
+		mov		sc_prm3, newcap
+		mov		sc_prm2, [this + CAPACITY]
+		mov		sc_prm1, [this + BUFFER]
+		mov		sc_num, SYSCALL_MREMAP
+		syscall								; buffer = mremap (buffer, cap, newcap, MREMAP_MAYMOVE)
+		test	buffer, buffer				; if (buffer < 0)
+		js		.error						;     then go to error branch
+;---[Update object properties]-------------
+		mov		this, [s_this]				; get "this" variable from the stack
+		mov		newcap, [s_ncap]			; get "newcap" variable from the stack
+		mov		[this + BUFFER], buffer		; this.buffer = buffer
+		mov		[this + CAPACITY], newcap	; this.capacity = newcap
+		add		stack, space				; restoring back the stack pointer
+		mov		status, 1					; return true
+		ret
+;---[Error branch]-------------------------
+.error:	add		stack, space				; restoring back the stack pointer
+		xor		status, status				; return false
+		ret
+
+;******************************************************************************;
+;       Constructor                                                            ;
+;******************************************************************************;
+Constructor:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+cap		equ		rsi							; object capacity
+;---[Internal variables]-------------------
+buffer	equ		rax							; pointer to memory buffer
+stack	equ		rsp							; stack pointer
+s_this	equ		stack + 0 * 8				; stack position of "this" variable
+s_cap	equ		stack + 1 * 8				; stack position of "cap" variable
+space	= 3 * 8								; stack size required by the procedure
+;------------------------------------------
+		sub		stack, space				; reserving stack size for local vars
+		mov		[s_this], this				; save "this" variable into the stack
+	Capacity	cap, buffer					; cap = Capacity (cap)
+		mov		[s_cap], cap				; save "cap" variable into the stack
+;---[Allocate memory for the object]-------
+		mov		sc_prm6, 0
+		mov		sc_prm5, -1
+		mov		sc_prm4, 0x22
+		mov		sc_prm3, 0x3
+		mov		sc_prm1, 0
+		mov		sc_num, SYSCALL_MMAP
+		syscall								; buffer = mmap (NULL, cap, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)
+		mov		this, [s_this]				; get "this" variable from the stack
+		mov		cap, [s_cap]				; get "cap" variable from the stack
+		test	buffer, buffer				; if (buffer < 0)
+		js		.error						;     then go to error branch
+;---[Normal execution branch]--------------
+		mov		[this + BUFFER], buffer		; this.buffer = buffer
+		mov		[this + CAPACITY], cap		; this.capacity = cap
+		mov		qword [this + SIZE], 0		; this.size = 0
+		add		stack, space				; restoring back the stack pointer
+		ret
+;---[Error branch]-------------------------
+.error:	mov		qword [this + BUFFER], 0		; this.buffer = NULL
+		mov		qword [this + CAPACITY], 0	; this.capacity = 0
+		mov		qword [this + SIZE], 0		; this.size = 0
+		add		stack, space				; restoring back the stack pointer
+		ret
+
+;******************************************************************************;
+;       Copy constructor                                                       ;
+;******************************************************************************;
+CopyConstructor:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to target accumulator object
+source	equ		rsi							; pointer to source accumulator object
+;---[Internal variables]-------------------
+buffer	equ		rax							; pointer to memory buffer
+temp	equ		rcx							; temporary register
+stack	equ		rsp							; stack pointer
+s_this	equ		stack + 0 * 8				; stack position of "this" variable
+s_src	equ		stack + 1 * 8				; stack position of "source" variable
+space	= 3 * 8								; stack size required by the procedure
+;------------------------------------------
+		sub		stack, space				; reserving stack size for local vars
+		cmp		this, source				; if (this == source)
+		je		.exit						;     then go to exit
+		mov		[s_this], this				; save "this" variable into the stack
+		mov		[s_src], source				; save "source" variable into the stack
+;---[Allocate memory for the object]-------
+		mov		sc_prm6, 0
+		mov		sc_prm5, -1
+		mov		sc_prm4, 0x22
+		mov		sc_prm3, 0x3
+		mov		sc_prm2, [source + CAPACITY]
+		mov		sc_prm1, 0
+		mov		sc_num, SYSCALL_MMAP
+		syscall								; buffer = mmap (NULL, source.capacity, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)
+		mov		this, [s_this]				; get "this" variable from the stack
+		mov		source, [s_src]				; get "source" variable from the stack
+		test	buffer, buffer				; if (buffer < 0)
+		js		.error						;     then go to error branch
+;---[Normal execution branch]--------------
+		mov		[this + BUFFER], buffer		; this.buffer = buffer
+		mov		temp, [source + CAPACITY]
+		mov		[this + CAPACITY], temp		; this.capacity = source.capacity
+		mov		temp, [source + SIZE]
+		mov		[this + SIZE], temp			; this.size = source.size
+;---[Copy content of nodes]----------------
+		mov		param3, [source + SIZE]
+		mov		param2, [source + BUFFER]
+		mov		param1, buffer
+		add		stack, space				; restoring back the stack pointer
+		jmp		Copy						; return Array::Copy (this.buffer, source.buffer, source.size)
+;---[Error branch]-------------------------
+.error:	mov		qword [this + BUFFER], 0	; this.buffer = NULL
+		mov		qword [this + CAPACITY], 0	; this.capacity = 0
+		mov		qword [this + SIZE], 0		; this.size = 0
+.exit:	add		stack, space				; restoring back the stack pointer
+		ret
+
+;******************************************************************************;
+;       Destructor                                                             ;
+;******************************************************************************;
+Destructor:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+;---[Internal variables]-------------------
+stack	equ		rsp							; stack pointer
+s_this	equ		stack + 0 * 8				; stack position of "this" variable
+space	= 1 * 8								; stack size required by the procedure
+;------------------------------------------
+		sub		stack, space				; reserving stack size for local vars
+		mov		[s_this], this				; save "this" variable into the stack
+		mov		sc_prm2, [this + CAPACITY]
+		mov		sc_prm1, [this + BUFFER]
+		mov		sc_num, SYSCALL_MUNMAP
+		syscall								; munmap (buffer, capacity)
+		mov		this, [s_this]				; get "this" variable from the stack
+		mov		qword [this + BUFFER], 0	; this.buffer = NULL
+		mov		qword [this + CAPACITY], 0	; this.capacity = 0
+		mov		qword [this + SIZE], 0		; this.size = 0
+		add		stack, space				; restoring back the stack pointer
+		ret
+
+;******************************************************************************;
+;       Accumulator functions                                                  ;
+;******************************************************************************;
+
+;==============================================================================;
+;       Reserve space into the buffer                                          ;
+;==============================================================================;
+Reserve:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+size	equ		rsi							; space to reserve into the buffer
+;---[Internal variables]-------------------
+result	equ		rax							; result register
+stack	equ		rsp							; stack pointer
+s_this	equ		stack + 0 * 8				; stack position of "this" variable
+space	= 1 * 8								; stack size required by the procedure
+;---[Check object capacity]----------------
+		add		size, [this + SIZE]			; size = this.size + size
+	Capacity	size, result				; size = Capacity (size)
+		cmp		size, [this + CAPACITY]		; if (size > this.capacity)
+		ja		.ext						;     then try to extend object capacity
+;---[Return pointer to reserved space]-----
+.back:	mov		result, [this + BUFFER]
+		add		result, [this + SIZE]		; return bufer + size
+		ret
+;---[Extend object capacity]---------------
+.ext:	sub		stack, space				; reserving stack size for local vars
+		mov		[s_this], this				; save "this" variable into the stack
+		call	Extend						; status = this.Extend (cap)
+		mov		this, [s_this]				; get "this" variable from the stack
+		add		stack, space				; restoring back the stack pointer
+		test	status, status				; if (status)
+		jnz		.back						;     then go back
+;---[Error branch]-------------------------
+.error:	xor		result, result				; return NULL
+		ret
+
+;==============================================================================;
+;       Fill reservered space into the buffer                                  ;
+;==============================================================================;
+Fill:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+size	equ		rsi							; size of data to add to the buffer
+;---[Internal variables]-------------------
+status	equ		al							; operation status
+;------------------------------------------
+		add		size, [this + SIZE]			; size = this.size + size
+		cmp		size, [this + CAPACITY]		; if (size > this.capacity)
+		ja		.error						;     then go to error branch
+;---[Normal execution branch]--------------
+		mov		[this + SIZE], size			; this.size += size
+		mov		status, 1					; return true
+		ret
+;---[Error branch]-------------------------
+.error:	xor		status, status				; return false
+		ret
+
+;==============================================================================;
+;       Get pointer to the buffer                                              ;
+;==============================================================================;
+GetData:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+;---[Internal variables]-------------------
+result	equ		rax							; result register
+;------------------------------------------
+		mov		result, [this + BUFFER]		; get pointer to the buffer
+		ret
+
+;==============================================================================;
+;       Clear the buffer                                                       ;
+;==============================================================================;
+Clear:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+;------------------------------------------
+		mov		qword [this + SIZE], 0		; this.size = 0
+		ret
+
+;******************************************************************************;
+;       Accumulator properties                                                 ;
+;******************************************************************************;
+macro	GET_PARAM	param
+{
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+;---[Internal variables]-------------------
+result	equ		rax							; result register
+;------------------------------------------
+		mov		result, [this + param]		; get object parameter
+		ret
+}
+GetCapacity:	GET_PARAM	CAPACITY
+GetSize:		GET_PARAM	SIZE
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+IsEmpty:
+;---[Parameters]---------------------------
+this	equ		rdi							; pointer to accumulator object
+;---[Internal variables]-------------------
+status	equ		al							; operation status
+size	equ		rdx							; object size
+;------------------------------------------
+		mov		size, [this + SIZE]			; get object size
+		test	size, size					; if (size == 0)
+		setz	status						;     return true
+		ret
+
+;###############################################################################
+;#                                 END OF FILE                                 #
+;###############################################################################
